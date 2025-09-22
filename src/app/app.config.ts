85af9df852e98898a202a5c0b6d5d3120ce74be0
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-09-22 13:44:31 -0600

    feat: update color palette and improve layout styles across components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -23,22 +23,21 @@ export const appConfig: ApplicationConfig = {
         preset: definePreset(Aura, {
           semantic: {
             primary: {
-              50: '{slate.50}',
-              100: '{slate.100}',
-              200: '{slate.200}',
-              300: '{slate.300}',
-              400: '{slate.400}',
-              500: '{slate.500}',
-              600: '{slate.600}',
-              700: '{slate.700}',
-              800: '{slate.800}',
-              900: '{slate.900}',
-              950: '{slate.950}',
+              50: '{rose.50}',
+              100: '{rose.100}',
+              200: '{rose.200}',
+              300: '{rose.300}',
+              400: '{rose.400}',
+              500: '{rose.500}',
+              600: '{rose.600}',
+              700: '{rose.700}',
+              800: '{rose.800}',
+              900: '{rose.900}',
+              950: '{rose.950}',
             },
           },
         }),
         options: {
-          darkModeSelector: '.my-app-dark',
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng',
