502b0ad7ab7b0587ff4d96f75cf1d7a32f6daf41
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-09-28 03:52:43 -0600

    feat: Add operations home and profile pages with routing and layout integration

---
+++ src/app/pages/ops/ops-home-page/ops-home-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ops-home-page',
+  imports: [],
+  templateUrl: './ops-home-page.html',
+  styleUrl: './ops-home-page.css'
+})
+export class OpsHomePage {
+
+}
--- src/app/routes/ops.routes.ts
+++ src/app/routes/ops.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { PrivateOpsLayout } from '../layouts/private-ops-layout/private-ops-layout';
+import { OpsProfilePage } from '../pages/ops/profile-page/profile-page';
+import { OpsHomePage } from '../pages/ops/ops-home-page/ops-home-page';
 
 export const OPS_ROUTES: Routes = [
   {
     path: '',
-    component: PrivateOpsLayout,
+    component: OpsHomePage,
   },
+  {
+    path: 'profile',
+    component: OpsProfilePage,
+  }
 ];
